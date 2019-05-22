e10803cb7a3a6fbaba24125d05d77cac96f78894
vonpower <vonpower@gmail.com>
2019-05-22 16:39:30 +0800

    'lite_for_run_Web_API'

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -7,7 +7,8 @@
 from question_classifier import *
 from question_parser import *
 from answer_search import *
-
+import web
+import json
 '''问答类'''
 class ChatBotGraph:
     def __init__(self):
@@ -16,7 +17,7 @@ class ChatBotGraph:
         self.searcher = AnswerSearcher()
 
     def chat_main(self, sent):
-        answer = '您好，我是小勇医药智能助理，希望可以帮到您。如果没答上来，可联系https://liuhuanyong.github.io/。祝您身体棒棒！'
+        answer = '您好，抱歉没能回答上来，祝您身体棒棒！'
         res_classify = self.classifier.classify(sent)
         if not res_classify:
             return answer
@@ -26,11 +27,32 @@ class ChatBotGraph:
             return answer
         else:
             return '\n'.join(final_answers)
+urls = (
+        '/', 'medical'
+    )
+chatbot = ChatBotGraph()
+class medical:
+    def __init__(self):
+        self.handler = chatbot
+    def POST(self):
+        data = json.loads(web.data())
+# '''   bearychat outgoing json example    
+#  {
+#   "token" : "995c98c6d79e22cc4200233e8def6f9c",
+#   "ts" : 1355517523,
+#   "text" : "!baike 中国",
+#   "trigger_word" : "!baike",
+#   "subdomain" : "your_domain",
+#   "channel_name" : "your_channel",
+#   "user_name" : "your_name"
+# }
+# '''
+        question = data["text"].replace(data["trigger_word"],"")
+        answer = self.handler.chat_main(question)
+        return f'{{"text" : "{answer}"\}}'
 
 if __name__ == '__main__':
-    handler = ChatBotGraph()
-    while 1:
-        question = input('用户:')
-        answer = handler.chat_main(question)
-        print('小勇:', answer)
+        app = web.application(urls, globals())
+        app.run()
+
 
